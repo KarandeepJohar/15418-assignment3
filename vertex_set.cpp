105e1c9a04c1fad2f033cdfd967bf6f8a94c0f52
Karandeep Johar <karandeepjohar@gmail.com>
2016-02-21 15:57:57 -0500

    incorrect new implementation

--- vertex_set.cpp
+++ vertex_set.cpp
@@ -9,60 +9,225 @@
 /**
  * Creates an empty VertexSet with the given type and capacity.
  * numNodes is the total number of nodes in the graph.
- * 
+ *
  * Student may interpret type however tey wish.  It may be helpful to
  * use different representations of VertexSets under different
  * conditions, and they different conditions can be indicated by 'type'
  */
+
+
+void packIndices(Vertex* output, Vertex* input, bool* boolArray, int n) {
+    Vertex* scratch = new Vertex[n];
+    Vertex* sums = new Vertex[n];
+    // prefix_sum(sums, boolArray, scratch, n);
+    for (int i = 0; i < n; ++i)
+    {
+        if (boolArray[i])
+        {
+            output[sums[i]] = input[i];
+        }
+    }
+    delete[] sums;
+    delete[] scratch;
+}
+
+void remDuplicates(Vertex* input, int size, int numNodes) {
+    Vertex* flags = new Vertex[numNodes];
+    #pragma omp parallel for
+    for (int i = 0; i < size; ++i)
+    {
+        flags[i] = -1;
+    }
+    for (int i = 0; i < size; ++i)
+    {
+        if (input[i] != -1 && flags[input[i]] == -1)
+        {
+            __sync_bool_compare_and_swap(&flags[input[i]], -1, i);
+        }
+    }
+    #pragma omp parallel for
+    for (int i = 0; i < size; ++i)
+    {
+        if (input[i] != -1)
+        {
+            if (flags[input[i]] == i)
+            {
+                flags[input[i]] = -1;
+            } else {
+                input[i] = -1;
+            }
+        }
+    }
+    delete[] flags;
+}
 VertexSet *newVertexSet(VertexSetType type, int capacity, int numNodes)
 {
-  VertexSet* vs = new VertexSet;
-  vs->size = 0;
-  vs->capacity = capacity;
-  vs->vertices.reserve(capacity);
-  vs->numNodes = numNodes;
-  vs->type = type;
-  // printf("capacity%d\n",capacity);
-  return vs;
+    VertexSet* vs = new VertexSet;
+    vs->numNodes = numNodes;
+    vs->size = 0;
+    vs->type = type;
+    vs->sparseUpToDate = false;
+    vs->denseUpToDate = false;
+    vs->capacity = capacity;
+    if (type == SPARSE)
+    {
+        vs->vertices = new Vertex[capacity];
+        vs->denseVertices = NULL;
+        vs->sparseUpToDate = true;
+
+    } else {
+        vs->denseVertices = new bool[numNodes];
+        vs->vertices = NULL;
+        vs->denseUpToDate = true;
+    }
+    // printf("capacity%d\n",capacity);
+    return vs;
+}
+
+VertexSet *newVertexSet(VertexSetType type, int capacity, int numNodes, bool* denseVertices) {
+    VertexSet* vs = new VertexSet;
+    vs->numNodes = numNodes;
+    vs->size = capacity;
+    vs->numNodes = numNodes;
+    vs->type = DENSE;
+    vs->denseUpToDate = true;
+    vs->sparseUpToDate = false;
+    vs->denseVertices = denseVertices;
+    vs->vertices = NULL;
+    return vs;
+}
+
+VertexSet *newVertexSet(VertexSetType type, int capacity, int numNodes, Vertex* vertices) {
+    VertexSet* vs = new VertexSet;
+    vs->numNodes = numNodes;
+    vs->size = capacity;
+    vs->numNodes = numNodes;
+    vs->type = SPARSE;
+    vs->denseUpToDate = false;
+    vs->sparseUpToDate = true;
+    vs->vertices = vertices;
+    vs->denseVertices = NULL;
+    return vs;
 }
 
 void freeVertexSet(VertexSet *set)
 {
 
-  // delete set->vertices;
-  delete set;
+    // delete set->vertices;
+    delete[] set->denseVertices;
+    delete[] set->vertices;
+    delete set;
+}
+
+void parallel_update_dense( bool* dense, Vertex* sparse,  int size, int numNodes) {
+    if (dense == NULL)
+    {
+        dense = new bool[numNodes];
+
+    }
+    #pragma omp parallel for
+    for (int i = 0; i < numNodes; ++i)
+    {
+        dense[i] = false;
+    }
+    #pragma omp parallel for
+    for (int i = 0; i < size ; ++i)
+    {
+        dense[sparse[i]] = true;
+    }
+}
+
+void parallel_pack_scan(Vertex* sparse, bool* dense, int size, int numNodes) {
+    if (sparse == NULL)
+    {
+        sparse = new Vertex(size);
+    }
+
+    #pragma omp parallel for
+    for (int i = 0; i < numNodes; ++i)
+    {
+        /* code */
+        //WRITE THE CODE HERE
+    }
+}
+
+
+void updateDense(VertexSet *set, bool convert = false) {
+    if (!(set->denseUpToDate))
+        parallel_update_dense(set->denseVertices, set->vertices, set->size, set->numNodes);
+    if (convert)
+        set->type = DENSE;
+    set->denseUpToDate = true;
+}
+
+void updateSparse(VertexSet *set, bool convert = false ) {
+    if (!(set->sparseUpToDate))
+        parallel_pack_scan(set->vertices, set->denseVertices,  set->size, set->numNodes);
+
+    if (convert)
+        set->type = SPARSE;
+
+    set->sparseUpToDate = true;
 }
 
 void addVertex(VertexSet *set, Vertex v)
-{ std::vector<Vertex>::iterator it;
+{   //this will convert it into dense
+    // updateDense(set, true);
+    if (set->type == DENSE)
+    {
+        if (set->denseVertices[v] == false)
+        {
+            set->size++;
+            ENSURES(set->size <= set->capacity);
+            set->denseVertices[v] = true;
 
-  it = std::find(set->vertices.begin(), set->vertices.end(), v);
-  if (it != set->vertices.end()) {
-    return;
-  }
-  ENSURES(set->size <= set->capacity);
-  set->vertices.push_back(v);
-  set->size = set->vertices.size();
+            set->denseUpToDate = true;
+            set->sparseUpToDate = false;
+        }
+
+        return;
+    }
+    else {
+        updateDense(set);
+        if (set->denseVertices[v] == false) {
+            set->size++;
+            ENSURES(set->size <= set->capacity);
+            set->denseVertices[v] = true;
+            set->denseUpToDate = true;
+            set->sparseUpToDate = false;
+            updateSparse(set);
+        }
+        return;
+    }
 }
 
 void removeVertex(VertexSet *set, Vertex v)
 {
-  ENSURES(set->size>0);
-  std::vector<Vertex>::iterator it;
+    ENSURES(set->size > 0);
+    //this will convert it into dense
+    // updateDense(set, true);
+    if (set->type == DENSE) {
 
-  it = std::find(set->vertices.begin(), set->vertices.end(), v);
+        if (set->denseVertices[v] == true) {
 
-  if (it != set->vertices.end()) {
-    using std::swap;
+            set->size--;
+            set->denseVertices[v] = false;
+            set->denseUpToDate = true;
+            set->sparseUpToDate = false;
+        }
 
-    // swap the one to be removed with the last element
-    // and remove the item at the end of the container
-    // to prevent moving all items after '5' by one
-    swap(*it, set->vertices.back());
-    set->vertices.pop_back();
-  }
-
-  set->size = set->vertices.size();
+        return;
+    } else {
+        updateDense(set);
+        if (set->denseVertices[v] == true) {
+            set->size--;
+            set->denseVertices[v] = false;
+            set->denseUpToDate = true;
+            set->sparseUpToDate = false;
+            updateSparse(set);
+        }
+        return;
+    }
 
 }
 
@@ -71,11 +236,11 @@ void removeVertex(VertexSet *set, Vertex v)
  */
 VertexSet* vertexUnion(VertexSet *u, VertexSet* v)
 {
-  // TODO: Implement
+    // TODO: Implement
 
-  // STUDENTS WILL ONLY NEED TO IMPLEMENT THIS FUNCTION IN PART 3 OF
-  // THE ASSIGNMENT
-  
-  return NULL;
+    // STUDENTS WILL ONLY NEED TO IMPLEMENT THIS FUNCTION IN PART 3 OF
+    // THE ASSIGNMENT
+
+    return NULL;
 }
 
