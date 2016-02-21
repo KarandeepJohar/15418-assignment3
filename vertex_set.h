7b2c2ff80d5dad52b48ef9a16da44710aef3a25b
Karandeep Johar <karandeepjohar@gmail.com>
2016-02-21 15:57:57 -0500

    incorrect new implementation

--- vertex_set.h
+++ vertex_set.h
@@ -4,23 +4,35 @@
 #include "graph.h"
 #include <vector>
 typedef enum {
-  SPARSE,
+	SPARSE,
+	DENSE,
 } VertexSetType;
 
 typedef struct {
-  int size;     // Number of nodes in the set
-  int numNodes; // Number of nodes in the graph
-  VertexSetType type; 
-  std::vector<Vertex> vertices;
-  int capacity;
+	int size;     // Number of nodes in the set
+	int numNodes; // Number of nodes in the graph
+	VertexSetType type;
+	// std::vector<Vertex> vertices;
+	bool* denseVertices;
+	bool denseUpToDate, sparseUpToDate;
+	Vertex* vertices;
+	int capacity;
 } VertexSet;
 
 VertexSet *newVertexSet(VertexSetType type, int capacity, int numNodes);
+VertexSet *newVertexSet(VertexSetType type, int capacity, int numNodes, bool* denseVertices);
+VertexSet *newVertexSet(VertexSetType type, int capacity, int numNodes, Vertex* vertices);
+void packIndices(Vertex* output, Vertex* input, bool* boolArray, int n);
+
+void remDuplicates(Vertex* input, int size, int numNodes);
+
 void freeVertexSet(VertexSet *set);
 
 void addVertex(VertexSet *set, Vertex v);
 void removeVertex(VertexSet *set, Vertex v);
 
 VertexSet*  vertexUnion(VertexSet *u, VertexSet* v);
+void updateDense(VertexSet *set, bool convert);
 
+void updateSparse(VertexSet *set, bool convert );
 #endif // __VERTEX_SET__
